c5fe3267b97e8e5cc7608fc6779d4af1e91408a4
EduarnyReyes <126604340+EduarnyReyes@users.noreply.github.com>
2023-02-28 14:53:33 -0300

    Add files via upload

---
+++ cv2.js
@@ -0,0 +1,5 @@
+
+document.getElementById("boton").onclick=function(){
+    console.log("capturamoseleventoclick");
+    document.getElementById("demo").innerHTML="Estamos probando nuestro pimer evento en js"
+}
